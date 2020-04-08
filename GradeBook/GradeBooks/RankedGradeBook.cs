8992ba9c5ea0f36144484c62ce20fbf23f1ed258
Kevin Sinclair <ksinclair@sgvinternational.com>
2020-04-08 10:54:02 -0500

    RankedGradeBook

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -9,6 +10,40 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5) throw new InvalidOperationException();
+
+            double oneFifth = Students.Count / 5;
+
+            var listOfGrades = Students.OrderBy(x => x.Grades);
+
+            double test = ((oneFifth * 4) /listOfGrades.Count() )*100;
+
+            if (averageGrade > (((oneFifth * 4) / listOfGrades.Count()) * 100))
+            {
+                return 'A';
+            }
+            else if (averageGrade > (((oneFifth * 3) / listOfGrades.Count()) * 100))
+            {
+                return 'B';
+            }
+            else if (averageGrade > (((oneFifth * 2) / listOfGrades.Count()) * 100))
+            {
+                return 'C';
+            }
+            else if (averageGrade > ((oneFifth / listOfGrades.Count()) * 100))
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
+
+
         }
     }
 }
