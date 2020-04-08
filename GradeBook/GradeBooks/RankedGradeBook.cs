1aea2635fe50fe1839b45dbf8417e25a56ba9d9d
Kevin Sinclair <ksinclair@sgvinternational.com>
2020-04-08 11:00:14 -0500

    2 more RankedGradeBookOverrides

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -20,7 +20,7 @@ namespace GradeBook.GradeBooks
 
             var listOfGrades = Students.OrderBy(x => x.Grades);
 
-            double test = ((oneFifth * 4) /listOfGrades.Count() )*100;
+            double test = ((oneFifth * 4) / listOfGrades.Count()) * 100;
 
             if (averageGrade > (((oneFifth * 4) / listOfGrades.Count()) * 100))
             {
@@ -45,5 +45,30 @@ namespace GradeBook.GradeBooks
 
 
         }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            }
+            else
+            {
+                base.CalculateStatistics();
+            }
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            }
+            else
+            {
+                base.CalculateStudentStatistics(name);
+            }
+        }
+
     }
 }
